2d5e3448360e0950ef79785e6eca89669538bedc
Devansh Kumar <devanshkumar9931@gmail.com>
2026-04-23 00:25:39 +0530

    added __init__.py in each folder

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os, sys
 from datetime import datetime
 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from vectordb.build_index import build_faiss_index
 from retrieval.retriever import retrieve_docs
